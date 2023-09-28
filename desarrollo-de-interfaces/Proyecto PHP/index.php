8f8b486376bf7a9aafb7adb00f441c620bcea46f
A26644 <a26644@sanvalero.net>
2023-09-28 15:36:24 +0200

    Updated DI

---
+++ desarrollo-de-interfaces/Proyecto PHP/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php include "./server/login.php" ?>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='./css/index.css'>
+    <script src='./script/js/index.js'></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
---
+++ desarrollo-de-interfaces/Proyecto PHP/script/js/index.js
@@ -0,0 +1,4 @@
+"use strict";
+onload = function () {
+    alert("hola");
+};
